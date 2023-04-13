1c02171383e0399b551b8fbc153e8a96077a8acc
tech-zjf <907572438@qq.com>
2023-04-13 11:57:58 +0800

    token

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,11 +12,15 @@ console.log("BASEURL", BASEURL);
 const request = (options: RequestOptions) => {
   const { urlRoute, method, data, complete } = options;
   const _url = BASEURL + urlRoute;
+  const token = Taro.getStorageSync("access_token");
   return new Promise((resolve, reject) => {
     Taro.request({
       url: _url,
       data,
       method,
+      header: {
+        Authorization: token,
+      },
       timeout: 2000,
       success(res) {
         resolve(res);
